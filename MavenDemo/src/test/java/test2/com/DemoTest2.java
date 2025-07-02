dde810b5053747c4c9f70ccdff1ffd2531366bcc
RavinaPawar <Rajkumar@LAPTOP-V843H2Q0>
2025-07-02 21:04:24 +0530

    DemoTest2 code is successfully run.

---
+++ MavenDemo/src/test/java/test2/com/DemoTest2.java
@@ -0,0 +1,18 @@
+package test2.com;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	@Test
+	
+	public void Test2() {
+		
+		System.out.println("I am in Test 2 ");
+		
+		
+		
+	}
+	
+
+}
