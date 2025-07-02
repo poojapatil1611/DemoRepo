38f167398830dcb297056b9b436f96bd480ee8cd
vyejgar <vyejgar@Dell-4193V93.AD.NSEIT.COM>
2025-07-02 23:38:47 +0530

    Practice Test code run successfully

---
+++ MavenDemo/src/test/java/test2/com/PracticeTest.java
@@ -0,0 +1,20 @@
+package test2.com;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+
+@Test
+	
+	public void Test4() {
+		
+		System.out.println("I am in Practice Test ");
+		
+
+}
+   
+   
+   
+   
+   
+}
